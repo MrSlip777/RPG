9657797b77ef7647dd4421abc47531c19db048c4
MrSlip777 <mrslipgamecreate01@gmail.com>
2018-03-31 12:42:56 +0900

    0331

--- Assets/Resources/Script/Effect/EffectManager.cs
+++ Assets/Resources/Script/Effect/EffectManager.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UniRx;
+using UniRx.Triggers;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EffectManager : MonoBehaviour {
 
@@ -38,6 +41,53 @@ public class EffectManager : MonoBehaviour {
             pos.y = (pos.y - (float)Screen.height/2)/mPixelPerUnit;
 
             prefab_Effect.transform.position = pos;
+        }
+    }
+}
+
+namespace Samples
+{
+    /// <summary>
+    /// エフェクトを生成する奴
+    /// </summary>
+    public class EffectManager : MonoBehaviour
+    {
+        [SerializeField]
+        private Button _button;
+
+        [SerializeField]
+        private ExplosionEffect _effectPrefab;
+
+        [SerializeField]
+        private Transform _hierarchyTransform; //追加
+
+        private EffectPool _effectPool; //追加
+
+        void Start()
+        {
+            //オブジェクトプールを生成
+            _effectPool = new EffectPool(_hierarchyTransform, _effectPrefab);
+
+            //破棄されたときにPoolを解放する
+            this.OnDestroyAsObservable().Subscribe(_ => _effectPool.Dispose());
+
+            //ボタンが押されたらエフェクト生成
+            _button.OnClickAsObservable()
+                .Subscribe(_ =>
+                {
+                    //ランダムな場所
+                    var position = new Vector3(Random.Range(-5f, 5f), 0, Random.Range(-5f, 5f));
+
+                    //poolから1つ取得
+                    var effect = _effectPool.Rent();
+
+                    //エフェクトを再生し、再生終了したらpoolに返却する
+                    effect.PlayEffect(position)
+                        .Subscribe(__ =>
+                        {
+                            _effectPool.Return(effect);
+                        });
+                });
         }
     }
 }
